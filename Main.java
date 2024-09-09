b3d0283e166608e0f8acf811e7e1904e4a2d9910
cmd-HMN <hafizmuhammadnoor01@gmail.com>
2024-09-09 23:03:18 +0500

    BoardAdded

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
-import engine.src.ChessBoard;
+import engine.src.ChessBoard.BitBoard;
 
-public class Main extends ChessBoard {
+public class Main extends BitBoard{
     public static void main(String[] args) {
-        ChessBoard.main(args);
+        BitBoard board = new BitBoard();
+
+        board.setPiece(0);
+
+        board.setPiece(63);
+
+        board.printBoard();
     }
 }
