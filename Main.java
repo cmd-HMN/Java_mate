1418424f92c4249cf688a8ee6c56de424d59a178
cmd-HMN <hafizmuhammadnoor01@gmail.com>
2024-09-09 18:52:01 +0500

    Corrected the files

--- Main.java
+++ Main.java
@@ -2,13 +2,6 @@ import engine.src.ChessBoard;
 
 public class Main extends ChessBoard {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
         ChessBoard.main(args);
-
-        System.out.println("Bye, World!");
-        System.out.println("Bye, World!");
-        System.out.println("Iman bhen ki lori");
-
     }
 }
