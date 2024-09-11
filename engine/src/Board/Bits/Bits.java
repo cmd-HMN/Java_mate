bbbbd58f10862b042c6676bf5dc6268e8e6bb6a9
cmd-HMN <hafizmuhammadnoor01@gmail.com>
2024-09-11 21:55:06 +0500

    Staged Changes

---
+++ engine/src/Board/Bits/Bits.java
@@ -0,0 +1,81 @@
+package engine.src.Board.Bits;
+
+public class Bits {
+
+    public static int int_64 = 64;
+
+    public static final long A1 = 1L << 0;
+    public static final long B1 = 1L << 1;
+    public static final long C1 = 1L << 2;
+    public static final long D1 = 1L << 3;
+    public static final long E1 = 1L << 4;
+    public static final long F1 = 1L << 5;
+    public static final long G1 = 1L << 6;
+    public static final long H1 = 1L << 7;
+
+    public static final long A2 = 1L << 8;
+    public static final long B2 = 1L << 9;
+    public static final long C2 = 1L << 10;
+    public static final long D2 = 1L << 11;
+    public static final long E2 = 1L << 12;
+    public static final long F2 = 1L << 13;
+    public static final long G2 = 1L << 14;
+    public static final long H2 = 1L << 15;
+
+    public static final long A3 = 1L << 16;
+    public static final long B3 = 1L << 17;
+    public static final long C3 = 1L << 18;
+    public static final long D3 = 1L << 19;
+    public static final long E3 = 1L << 20;
+    public static final long F3 = 1L << 21;
+    public static final long G3 = 1L << 22;
+    public static final long H3 = 1L << 23;
+
+    public static final long A4 = 1L << 24;
+    public static final long B4 = 1L << 25;
+    public static final long C4 = 1L << 26;
+    public static final long D4 = 1L << 27;
+    public static final long E4 = 1L << 28;
+    public static final long F4 = 1L << 29;
+    public static final long G4 = 1L << 30;
+    public static final long H4 = 1L << 31;
+
+    public static final long A5 = 1L << 32;
+    public static final long B5 = 1L << 33;
+    public static final long C5 = 1L << 34;
+    public static final long D5 = 1L << 35;
+    public static final long E5 = 1L << 36;
+    public static final long F5 = 1L << 37;
+    public static final long G5 = 1L << 38;
+    public static final long H5 = 1L << 39;
+
+    public static final long A6 = 1L << 40;
+    public static final long B6 = 1L << 41;
+    public static final long C6 = 1L << 42;
+    public static final long D6 = 1L << 43;
+    public static final long E6 = 1L << 44;
+    public static final long F6 = 1L << 45;
+    public static final long G6 = 1L << 46;
+    public static final long H6 = 1L << 47;
+
+    public static final long A7 = 1L << 48;
+    public static final long B7 = 1L << 49;
+    public static final long C7 = 1L << 50;
+    public static final long D7 = 1L << 51;
+    public static final long E7 = 1L << 52;
+    public static final long F7 = 1L << 53;
+    public static final long G7 = 1L << 54;
+    public static final long H7 = 1L << 55;
+
+    public static final long A8 = 1L << 56;
+    public static final long B8 = 1L << 57;
+    public static final long C8 = 1L << 58;
+    public static final long D8 = 1L << 59;
+    public static final long E8 = 1L << 60;
+    public static final long F8 = 1L << 61;
+    public static final long G8 = 1L << 62;
+    public static final long H8 = 1L << 63;
+
+
+    public static final long[] SQUARES = new long[int_64];
+}
--- engine/src/Board/ChessPieces/Pawn.java
+++ engine/src/Board/ChessPieces/Pawn.java
@@ -4,16 +4,58 @@ import engine.src.Board.Moves.Moves;
 
 public class Pawn{
 
-    private long white_single_forward(long move, long empty){
+    private long wPawn_forward(long move, long empty){
         return Moves.north(move) & empty;
     };
 
-    private long black_single_forward(long move, long empty){
+    private long bPawn_forward(long move, long empty){
         return Moves.north(move) & empty;
     }
 
-    private long white_double_forward(long move, long empty){
-        return Moves.north(move);
+    private long wPawn_DForward(long move, long empty){
+        long forward = wPawn_forward(move, empty);
+        return Moves.north(forward) & empty;
+    }
+
+    private long bPawn_DForward(long move, long empty){
+        long forward = bPawn_forward(move, empty);
+        return Moves.north(forward) & empty;
+    }
+
+    private long wPawn_capture(long move){
+        return Moves.north_east(move) | Moves.north_west(move);
+    }
+
+    private long bPawn_capture(long move){
+        return Moves.south_east(move) | Moves.south_west(move);
+    }
+
+    public long white_get_possible_piece(long move, long empty){
+
+        long single_forward = wPawn_forward(move, empty);
+
+        long double_forward = wPawn_DForward(move, empty);
+
+        return single_forward | double_forward ;
+
+    }
+
+    public long black_get_possible_piece(long move, long empty){
+
+        long single_forward = bPawn_forward(move, empty);
+
+        long double_forward = bPawn_DForward(move, empty);
+
+        return single_forward | double_forward;
+
+    }
+
+    public long white_possible_attack(long move){
+        return wPawn_capture(move);
+    }
+
+    public long black_possible_attack(long move){
+        return bPawn_capture(move);
     }
 
 }
