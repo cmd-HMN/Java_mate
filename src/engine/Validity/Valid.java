d5e1cc748cbf200c70c16f3b20c7e18a942fb35c
cmd-HMN <hafizmuhammadnoor01@gmail.com>
2024-09-19 20:54:52 +0500

    En Passant added somehow

--- src/engine/Validity/Valid.java
+++ src/engine/Validity/Valid.java
@@ -8,16 +8,10 @@ public class Valid {
     //to check if the pawn has move double squares
     public boolean isDoubleSquare(long from, long to, PlayerColor playerColor){
         if(playerColor == PlayerColor.WHITE){
-            System.out.println("White Pawn Move");
-            System.out.println("From: " + Long.toBinaryString(from));
-            System.out.println("To: " + Long.toBinaryString(to));
             return (from & 0x000000000000FF00L) != 0 && (to & 0x00000000FF000000L) != 0;
         }
         else{
-            System.out.println("Black Pawn Move");
-            System.out.println("From: " + Long.toBinaryString(from));
-            System.out.println("To: " + Long.toBinaryString(to));
-            return (from & 0x00FF000000000000L) != 0 && (to & 0xFF00000000000000L) != 0;
+            return (from & 0x00FF000000000000L) != 0 && (to & 0x000000FF00000000L) != 0;
         }
     }
 }
