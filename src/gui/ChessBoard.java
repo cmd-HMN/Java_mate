87b179912c5be5b0ca219f659a2fa184b79048fe
cmd-HMN <hafizmuhammadnoor01@gmail.com>
2024-11-09 13:38:09 +0500

    Os setting testing

--- src/gui/ChessBoard.java
+++ src/gui/ChessBoard.java
@@ -2,6 +2,7 @@ package src.gui;
 
 import javax.swing.SwingUtilities;
 
+// changed os setting
 public class ChessBoard {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(BoardFrame::new);
